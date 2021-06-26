c1afa21028aab7f05c7ff28cf2552694a9560f19
SMSNSenarath <smsnshakilaa372@gmail.com>
2021-06-26 15:56:27 +0530

    Changed all work UI

--- src/elements/Work/Work.js
+++ src/elements/Work/Work.js
@@ -19,78 +19,35 @@ class Work extends Component {
     });
   }
 
-  renderWorks = (works) => {
+  renderAllWorks = (works) => {
     return (
-      <div>
+      <div className="row">
         {works.map((work, index) => {
-          const posterId = work.postedBy ? work.postedBy._id : "No Id";
-          const posterName = work.postedBy ? work.postedBy.name : "";
-          const posterPhone = work.postedBy ? work.postedBy.phone : "";
-          // <p>{new Date(work.created).toDateString()}</p>;
-          // const posterProfPic = work.postedBy
-          //   ? work.postedBy.profilePicture
-          //   : "";
-
           return (
-            <div
-              className="card"
-              style={{ textAlign: "left", marginBottom: "2rem" }}
-            >
-              <div className="row">
-                <div className="col-md-4">
-                  <img
-                    className="card-img-top"
-                    src={work.images[0]}
-                    alt="image1"
-                    width="auto"
-                    height="500px"
-                  />
+            <div class="card" style={{ width: "18rem", marginRight: "2rem" }}>
+              <div>
+                <img
+                  class="card-img-top"
+                  src={work.images[0]}
+                  style={{ height: "12rem" }}
+                  alt="Card image cap"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">{work.title}</h5>
+                  <p class="card-text">
+                    {work.area} | {work.category}{" "}
+                  </p>
+                  <p class="card-text"></p>
+                  <Link
+                    to={`/works/${work._id}`}
+                    className="btn btn-primary mr-3"
+                  >
+                    View Work
+                  </Link>
+                  <a href="/hirer-dashboard" className="btn btn-danger">
+                    Rate Work
+                  </a>
                 </div>
-                <div className="col-md-4">
-                  <img
-                    className="card-img-top"
-                    src={work.images[1]}
-                    alt="image2"
-                    width="auto"
-                    height="500px"
-                  />
-                </div>
-                <div className="col-md-4">
-                  <img
-                    className="card-img-top"
-                    src={work.images[2]}
-                    alt="image3"
-                    width="auto"
-                    height="500px"
-                  />
-                </div>
-              </div>
-
-              <div className="card-body">
-                <h5 className="card-title">{work.title}</h5>
-                <p className="card-subtitle mb-2 text-muted">
-                  Rs. {work.fee} per day
-                </p>
-                <p className="card-subtitle mb-2 text-muted">{work.category}</p>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">Phone No - {posterPhone}</li>
-                <li className="list-group-item">
-                  Worker Name -{" "}
-                  <Link to={`/worker/${posterId}`}>{posterName}</Link>
-                </li>
-                <li className="list-group-item">Area - {work.area}</li>
-              </ul>
-              <div className="card-body">
-                <Link
-                  to={`/works/${work._id}`}
-                  className="btn btn-primary mr-3"
-                >
-                  View Work
-                </Link>
-                <a href="/hirer-dashboard" className="btn btn-danger">
-                  Rate Work
-                </a>
               </div>
             </div>
           );
@@ -106,7 +63,7 @@ class Work extends Component {
       <div className="container">
         <h2 style={{ textAlign: "left" }}>All Works</h2>
         <hr />
-        <div>{this.renderWorks(works)}</div>
+        <div>{this.renderAllWorks(works)}</div>
       </div>
     );
   }
